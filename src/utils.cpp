6d1f267163e97f03f028e43e9b4a647ec84eb5ee
Bootj05 <116076783+Bootj05@users.noreply.github.com>
2025-06-08 01:04:49 +0200

    Fix utils include path

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,7 +1,7 @@
 // Copyright 2025 Bootj05
 //
 // Licensed under the MIT License.
-#include "include/utils.h"
+#include "utils.h"  // NOLINT(build/include_subdir)
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
